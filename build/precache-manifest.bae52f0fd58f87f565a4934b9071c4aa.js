d19aea522541be6047dcb6578e044dcfd83e84ae
Berk Hüseyin Gaffaroğlu <berkgaffaroglu@Berks-MacBook-Pro.local>
2020-08-19 02:36:22 +0300

    build

---
+++ build/precache-manifest.bae52f0fd58f87f565a4934b9071c4aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23382e48df4ee22a621fc21787a6b65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf21f624da21708d9552",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e7ab853048b1d7ac6e2f",
+    "url": "/static/css/main.660466bd.chunk.css"
+  },
+  {
+    "revision": "cf21f624da21708d9552",
+    "url": "/static/js/2.07593ad4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.07593ad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ab853048b1d7ac6e2f",
+    "url": "/static/js/main.e00201f1.chunk.js"
+  },
+  {
+    "revision": "afbaf1b86eb5ed055c4b",
+    "url": "/static/js/runtime-main.3717989d.js"
+  },
+  {
+    "revision": "a6e5446a7c5789aabc9b37eaaf72134d",
+    "url": "/static/media/Oswald.a6e5446a.ttf"
+  },
+  {
+    "revision": "7810feee2a587264c0d6058d52b60ae4",
+    "url": "/static/media/Pangolin-Regular.7810feee.ttf"
+  },
+  {
+    "revision": "3b5635ac5d39c28200bf3993d90d09c7",
+    "url": "/static/media/Raleway.3b5635ac.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
